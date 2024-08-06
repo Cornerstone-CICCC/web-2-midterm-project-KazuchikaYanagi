f19a1427c23d2c7ac1687d6de1766db045dec272
Kazuchika Yanagi <yanagikazuchika@gmail.com>
2024-08-06 15:45:30 -0700

    added: home icon in public folder

--- moviepicker/src/components/header.jsx
+++ moviepicker/src/components/header.jsx
@@ -1,11 +1,17 @@
 import React from "react";
+import homeIcon from "../../public/movie_picker_darkpurple.svg";
 
 const header = () => {
-  return;
-  <div>
-    <h1>hi</h1>
-    <img src="../imgs/movie_picker_darkpurple.png" alt="" />
-  </div>;
+  return (
+    <>
+      <nav>
+        <img src={homeIcon} alt="homeIcon" />
+        <ul>
+          <li>ABOUT</li>
+        </ul>
+      </nav>
+    </>
+  );
 };
 
 export default header;
